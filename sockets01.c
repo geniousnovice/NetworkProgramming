d11e6992d2fb0e17d9fc5484dbfc487994ef4b19
geniousnovice <48413694+geniousnovice@users.noreply.github.com>
2019-03-11 01:40:45 +0900

    Create sockets01.c

---
+++ sockets01.c
@@ -0,0 +1,22 @@
+#pragma comment(lib, "ws2_32.lib")
+#include <stdio.h>
+#include <WinSock2.h>
+
+int main(int argc, char* argv[])
+{
+	WSADATA windowsSocketsAPIData;
+	
+	// Start socket programming
+	if (WSAStartup(MAKEWORD(2, 2), &windowsSocketsAPIData) != 0)
+	{
+		printf("STATE = FAILURE from WSAStartup() : %d", WSAGetLastError());
+	}
+
+	printf("STATE : SUCCESS from WSAStartup()");
+
+	// Finish socket programming
+	if (WSACleanup() == SOCKET_ERROR)
+	{
+		printf("STATE = FAILURE from WSACleanup() : %d", WSAGetLastError());
+	}
+}
